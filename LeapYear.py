561edd0129557e768a9eecfecb92c57939cbd5a6
badroyahya9 <badroyahya9@gmail.com>
2024-07-20 21:05:12 +0100

    Leap year check

---
+++ LeapYear.py
@@ -0,0 +1,20 @@
+# Which year do you want to check?
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0 :
+  if year % 100 == 0 :
+    if year % 400 == 0:
+      print("Leap year ")
+    else : 
+      print("Not leap year")  
+  else :
+    print("Leap year")
+else :
+  if year % 100 == 0 :
+    print("Not leap year")
+  else :
+    if year % 400 == 0 :
+      print("Leap year")
+    else :
+      print("Not leap year")
+      
